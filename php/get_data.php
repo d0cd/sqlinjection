dd6e2191affebcad9b8b6612ee482b2fbf8941f5
Pranav Gaddamadugu <pranavsaig@gmail.com>
2017-04-29 14:02:52 -0700

    php files

---
+++ php/get_data.php
@@ -0,0 +1,39 @@
+<?php
+
+$servername = "127.0.0.1";
+$username = "root";
+$password = "Gforce98!";
+$dbname = "store";
+
+$connect = mysqli_connect($servername, $username, $password, $dbname);
+if (!$connect) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+$sql = "SELECT * FROM secrets WHERE Num=?";
+
+$stmt = mysqli_prepare($connect, $sql);
+mysqli_stmt_bind_param($stmt, 'i', $num);
+
+$num = $_GET["num"];
+
+
+//Don't need to insert id since it's an PRI_KEY A_I
+mysqli_stmt_execute($stmt)
+or die (mysqli_error($connect));
+
+mysqli_stmt_bind_result($stmt, $firstname, $lastname, $secretmessage, $num);
+
+echo "<table>"; // start a table tag in the HTML
+
+while(mysqli_stmt_fetch($stmt)){   //Creates a loop to loop through results
+echo "<tr><td>" . $firstname . "</td><td>" . $lastname . "</td><td>" . $secretmessage . "</td><td>" . $num . "</td></tr>";  //$row['index'] the index here is a field name
+}
+
+echo "</table>"; //Close the table in HTML
+
+mysqli_stmt_close($stmt);
+
+mysqli_close($connect);
+?>
---
+++ php/send_data.php
@@ -0,0 +1,35 @@
+<?php
+
+$servername = "127.0.0.1";
+$username = "root";
+$password = "Gforce98!";
+$dbname = "store";
+
+$connect = mysqli_connect($servername, $username, $password, $dbname);
+if (!$connect) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+
+$sql = "INSERT INTO secrets (FirstName, LastName, SecretMessage, Num) 
+			VALUES (?,?,?,?)";
+
+$stmt = mysqli_prepare($connect, $sql);
+mysqli_stmt_bind_param($stmt, 'sssi', $firstname, $lastname, $secret, $num);
+
+$firstname = $_POST["FirstName"];
+$lastname = $_POST["LastName"];
+$secret = $_POST["SecretMessage"];
+$num = $_POST["Num"];
+
+
+mysqli_stmt_execute($stmt);
+
+mysqli_stmt_close($stmt);
+
+
+//Don't need to insert id since it's an PRI_KEY A_I
+mysqli_close($connect);
+header("location: form.html");
+?>
